0a3f553d70e924fcaf531471d71c30e4da148f1e
Sanjeev Midda <s.midda1@gmail.com>
2026-09-06 22:26:14 +0100

    chore: add page and navigation component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" />
+      <Route path="/recipie" />
+      <Route path="/favourites" />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
