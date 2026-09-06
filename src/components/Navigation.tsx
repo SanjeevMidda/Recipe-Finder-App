457c081cb21071faf495d434eb383358cfa9a4f5
Sanjeev Midda <s.midda1@gmail.com>
2026-09-06 22:52:57 +0100

    feat: add navigation links and active styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <div className="navigation">
+      <div className="logo"></div>
+
+      <nav>
+        <NavLink className="nav-link" to="/" end>
+          Home
+        </NavLink>
+        <NavLink className="nav-link" to="/search">
+          Search
+        </NavLink>
+        <NavLink className="nav-link" to="/favourites">
+          Favourites
+        </NavLink>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
