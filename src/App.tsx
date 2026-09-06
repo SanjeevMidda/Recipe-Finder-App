94086889621beda48d69d0b53915be1d5e738dc4
Sanjeev Midda <s.midda1@gmail.com>
2026-09-06 22:31:23 +0100

    feat: set up React Router routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Favourites from "./components/Favourites";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
 
 function App() {
   return;
   <BrowserRouter>
+    <Navigation />
+
     <Routes>
-      <Route path="/" />
-      <Route path="/recipie" />
-      <Route path="/favourites" />
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/favourites" element={<Favourites />} />
     </Routes>
   </BrowserRouter>;
 }
