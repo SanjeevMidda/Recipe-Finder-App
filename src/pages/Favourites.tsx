30644e6642704696e251d8343a8738767abe64cc
Sanjeev Midda <s.midda1@gmail.com>
2026-09-08 21:30:47 +0100

    feat: create favourites page

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,11 +1,33 @@
+import RecipeCard from "../components/RecipeCard";
 import type { Meal } from "../types/Meal";
 
-type FavouriteProps = {
+type FavouritesProps = {
   favourites: Meal[];
   removeFavourite: (recipeId: string) => void;
 };
-const Favourites = ({ favourites, removeFavourite }: FavouriteProps) => {
-  return <div></div>;
+
+const Favourites = ({ favourites, removeFavourite }: FavouritesProps) => {
+  return (
+    <div className="favouritesContainer">
+      <h1>Favourites</h1>
+
+      {favourites.length === 0 && (
+        <p>You don't have any favourite recipes yet.</p>
+      )}
+
+      {favourites.length > 0 && (
+        <div>
+          {favourites.map((recipe) => (
+            <RecipeCard
+              key={recipe.idMeal}
+              recipe={recipe}
+              removeFavourite={removeFavourite}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Favourites;
